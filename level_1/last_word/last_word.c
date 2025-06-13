9c8fc5d7677c49506217da9e609cd7b35242b47b
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-06-13 11:58:56 +0200

    last_word done

--- level_1/last_word/last_word.c
+++ level_1/last_word/last_word.c
@@ -6,7 +6,41 @@
 /*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 11:38:33 by fmoulin           #+#    #+#             */
-/*   Updated: 2025/06/13 11:38:34 by fmoulin          ###   ########.fr       */
+/*   Updated: 2025/06/13 11:55:39 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	last_word(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	while (str[len] != 32 && str[len] != 9)
+		len--;
+	while (str[len])
+	{
+		write(1, &str[len], 1);
+		len++;
+	}	
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		last_word(argv[1]);
+	}
+	return (0);
+}
